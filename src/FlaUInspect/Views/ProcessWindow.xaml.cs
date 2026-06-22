dd62dc79a6436ec3ad9fbea47d9c4c834ae5e84d
EXT-Gweltaz <ext.g.lemartret@lacroix.group>
2026-06-22 19:34:03 +0200

    added "Bring to front" contextmenu

--- src/FlaUInspect/Views/ProcessWindow.xaml.cs
+++ src/FlaUInspect/Views/ProcessWindow.xaml.cs
@@ -70,11 +70,16 @@ public partial class ProcessWindow : Window {
 	}
 
 	private void TreeViewControl_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e) {
-		if (DataContext is not ProcessViewModel processViewModel ||
-			TreeViewControl.SelectedItem is not ElementViewModel selectedElement)
+		if (DataContext is not ProcessViewModel processViewModel || TreeViewControl.SelectedItem is not ElementViewModel selectedElement)
 			return;
 
-		processViewModel.SetFocus(selectedElement);
-		ProcessViewModel.BlinkElement(selectedElement);
+		processViewModel.SetFocus(selectedElement, 3);
+	}
+
+	private void HighlightFocusClick(object sender, RoutedEventArgs e) {
+		if (DataContext is not ProcessViewModel processViewModel || TreeViewControl.SelectedItem is not ElementViewModel selectedElement)
+			return;
+
+		processViewModel.SetFocus(selectedElement, 3);
 	}
 }
